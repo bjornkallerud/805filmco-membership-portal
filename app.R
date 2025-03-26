78d4e16e89f27af6b36ec13362ee8b511eb26a40
Bjorn Kallerud <bjornkallerud@gmail.com>
2025-03-26 12:17:49 -0700

    turned logo into moving gif

--- app.R
+++ app.R
@@ -131,7 +131,7 @@ ui <- dashboardPage(
   title = "Membership Database",
   dashboardHeader(
     title = tags$div(
-      tags$img(src = "805filmco-logo.png", height = 50),
+      tags$img(src = "805LogoLooped.gif", height = 50),
       style = "color: white; font-family:'Hammersmith One'; font-size:30px"
     ),
     titleWidth = 250,
